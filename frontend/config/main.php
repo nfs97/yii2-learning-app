7c4ba6d62b62a011340a0a2960184b9a63cf0f0a
Vlady <vladronaldo7@gmail.com>
2016-10-01 16:43:07 +0300

    learned how to make search in CRUD app and made my first module

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -11,6 +11,11 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
+    'modules' => [
+        'settings' => [
+            'class' => 'frontend\modules\settings\Settings',
+        ],
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
